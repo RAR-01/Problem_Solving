d9c41204f1ed28335eb396f746e1514acb232887
Rishi Aditya Rai <rishiaditya19@gmail.com>
2026-05-13 23:25:44 +0530

    slight change in practice 6 solution

--- C++/practice6.cpp
+++ C++/practice6.cpp
@@ -32,3 +32,10 @@ int main () {
     cout << k;
     return 0;
 }
+//this only counts the no of distinct but the question asks more
+// int count = 1;
+// for (int i = 0; i < arr.size(); i++){
+//     if(arr[i] != arr[i+1]){
+//         count++;
+//     }
+// }
