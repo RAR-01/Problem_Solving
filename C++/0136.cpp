5907f8a5866c47ee8bcc5d30d250180f56c3343d
Rishi Aditya Rai <rishiaditya19@gmail.com>
2026-07-02 23:28:50 +0530

    leetcode problem no 136 more optimised solution

--- C++/0136.cpp
+++ C++/0136.cpp
@@ -18,3 +18,17 @@ public:
         }
     }
 };
+// more optimised approach 
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        
+        int ans = 0;
+
+        for(int num : nums){
+            ans ^= num;
+        }
+
+        return ans;
+    }
+};
