9d284bf828fd1811654235d6640c6ef438c88faa
Rishi Aditya Rai <rishiaditya19@gmail.com>
2026-07-02 23:23:03 +0530

    leetcode problem no 136

---
+++ C++/0136.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        
+        unordered_map<int, int> freq;
+
+        for(int num : nums){
+            freq[num]++;
+        }
+
+        for(int num : nums){
+            if(freq[num] == 1){
+                return num;
+            }
+        }
+    }
+};
