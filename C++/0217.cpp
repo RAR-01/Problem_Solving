bb7a3c4dec2ff7babbee9d9ed9b088fffdfc1228
Rishi Aditya Rai <rishiaditya19@gmail.com>
2026-07-06 22:01:07 +0530

    leetcode problem no 217

---
+++ C++/0217.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//gives tle
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        
+        for(int i = 0; i < nums.size(); i++){
+            for(int j = i + 1; j < nums.size(); j++){
+                if(nums[i] == nums[j]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
+
+// using unordered_map
