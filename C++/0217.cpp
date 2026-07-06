54570d2ab7c68a8fed4e79bce3387259f394c688
Rishi Aditya Rai <rishiaditya19@gmail.com>
2026-07-06 22:07:08 +0530

    leetcode problem no 217

--- C++/0217.cpp
+++ C++/0217.cpp
@@ -36,3 +36,18 @@ public:
 };
 
 //unordered_set
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        
+        unordered_set<int> seen;
+
+        for(int num : nums){
+            if(seen.find(num) != seen.end()){
+                return true;
+            }
+            seen.insert(anum);
+        }
+        return false;
+    }
+};
