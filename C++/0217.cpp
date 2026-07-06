fbef3d182e6bfb747fb5f1ab1361783ac0c21788
Rishi Aditya Rai <rishiaditya19@gmail.com>
2026-07-06 22:03:43 +0530

    problem no 217 using unordered_map

--- C++/0217.cpp
+++ C++/0217.cpp
@@ -18,3 +18,21 @@ public:
 };
 
 // using unordered_map
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        
+        unordered_map<int, int>freq;
+
+        for(int num : nums){
+            freq[num]++;
+
+            if(freq[num] > 1){
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+//unordered_set
