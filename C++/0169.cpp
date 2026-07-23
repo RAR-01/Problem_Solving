98d91df680e096f5fc493c6475867ca0568ff9e2
Rishi Aditya Rai <rishiaditya19@gmail.com>
2026-07-23 22:38:27 +0530

    leetcode problem no 169

---
+++ C++/0169.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        
+        int n = nums.size();
+
+        unordered_map<int,int> freq;
+
+        for(auto num : nums){
+            freq[num]++;
+
+            if(freq[num] > n / 2){
+                return num;
+            }
+        }
+        return -1;
+    }
+};
